5abc01155763d8f7bdf77e810ab02fc72c4cd292
letzbug <129280428+letzbug@users.noreply.github.com>
2026-08-05 13:52:45 +0200

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -13,19 +13,19 @@ window.UNIPOP_CONFIG = {
       time: "18h30 – 20h00",
       place: "Centre culturel, Ettelbruck",
       trainer: "Laura Müller",
-      image: "assets/images/course-placeholder.svg",
+      image: "assets/images/yoga-demo.jpg",
       url: "https://www.unipop.lu/",
       available: true
     },
     {
       id: "cuisine-2026-10-21",
-      title: "CUISINE",
-      subtitle: "Saveurs et découvertes",
-      date: "21 octobre 2026",
-      time: "18h00 – 21h00",
-      place: "Espace cuisine, Nordstad",
+      title: "YOGA",
+      subtitle: "Bien-être et équilibre",
+      date: "14 octobre 2026",
+      time: "18h30 – 20h00",
+      place: "Centre culturel, Ettelbruck",
       trainer: "UniPop",
-      image: "assets/images/course-placeholder.svg",
+      image: "assets/images/yoga-demo.jpg",
       url: "https://www.unipop.lu/",
       available: true
     }
